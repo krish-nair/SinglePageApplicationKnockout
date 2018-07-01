8c20fd3145c3cd76d1ac06b69af79b41546b79b3
krish-nair <krish.nair101@gmail.com>
2018-07-01 13:50:00 +1200

    AddTechnologyTbl

---
+++ SinglePageApplicationKnockout/Models/Technology.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SinglePageApplicationKnockout.Models
+{
+    public class Technology
+    {
+        public virtual int TechnologyId{ get; set; }
+        public virtual string TechnologyName{ get; set; }
+    }
+}
